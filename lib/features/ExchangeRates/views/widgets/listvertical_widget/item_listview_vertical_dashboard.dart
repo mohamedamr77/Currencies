35ada5e55b0a115de690d6419bef3b733ae1a39c
mohamedamr77 <moamr947@gmail.com>
2024-07-09 11:41:36 +0300

    make in click in item listview vertical go to details

--- lib/features/ExchangeRates/views/widgets/listvertical_widget/item_listview_vertical_dashboard.dart
+++ lib/features/ExchangeRates/views/widgets/listvertical_widget/item_listview_vertical_dashboard.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import '../../../../../core/color.dart';
 import '../../../../../core/textstyle.dart';
 import '../../../../../core/shared_widget/country_currency.dart';
+import '../../../../DetialsScreen/screen.dart';
 
 class ItemListviewVerticalDashboard extends StatelessWidget {
   const ItemListviewVerticalDashboard({super.key, required this.widget, required this.sellingPrice, required this.buyingPrice, required this.nameWidget});
@@ -11,46 +12,51 @@ final double buyingPrice;
 final String nameWidget;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 10),
-      padding: const EdgeInsets.all(10),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(9),
-      ),
-      child:  Row(
-        textDirection: TextDirection.rtl,
-        children: [
-          CircleAvatar(
-            child: widget,
-          ),
-          const SizedBox(width: 10),
-          CountryCurrency(nameWidget, fontSize: 16,),
-          const Spacer(flex: 2,),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text(
-                "$sellingPrice", // Replace with the actual buying price
-                style: const CustomTextStyle(
-                  color: ColorApp.deebBlueTextColor,
-                  fontFamily: "Tajawal",
-                  fontSize: 16,
+    return GestureDetector(
+      onTap: (){
+        Navigator.push(context, MaterialPageRoute(builder: (context) => DetailsScreen(),));
+      },
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 10),
+        padding: const EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(9),
+        ),
+        child:  Row(
+          textDirection: TextDirection.rtl,
+          children: [
+            CircleAvatar(
+              child: widget,
+            ),
+            const SizedBox(width: 10),
+            CountryCurrency(nameWidget, fontSize: 16,),
+            const Spacer(flex: 2,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  "$sellingPrice", // Replace with the actual buying price
+                  style: const CustomTextStyle(
+                    color: ColorApp.deebBlueTextColor,
+                    fontFamily: "Tajawal",
+                    fontSize: 16,
+                  ),
                 ),
-              ),
-              const SizedBox(width: 62),
-              Text(
-                "$buyingPrice", // Replace with the actual selling price
-                style: const CustomTextStyle(
-                  color: ColorApp.deebBlueTextColor,
-                  fontFamily: "Tajawal",
-                  fontSize: 16,
+                const SizedBox(width: 62),
+                Text(
+                  "$buyingPrice", // Replace with the actual selling price
+                  style: const CustomTextStyle(
+                    color: ColorApp.deebBlueTextColor,
+                    fontFamily: "Tajawal",
+                    fontSize: 16,
+                  ),
                 ),
-              ),
-            ],
-          ),
-        ],
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
