dc4b78a7f07059128d5864fb89f4e41230ae41cc
mohamedamr77 <moamr947@gmail.com>
2024-08-06 23:38:28 +0300

    convert stless to stfull in echangerates screen

--- lib/features/ExchangeRates/presentation/views/widgets/exchange_rates_body.dart
+++ lib/features/ExchangeRates/presentation/views/widgets/exchange_rates_body.dart
@@ -6,14 +6,8 @@ import '../../../../../core/shared_widget/three_Text_bank_buying_selling.dart';
 import 'listvertical_widget/list_vertical_banks_selling_buying.dart';
 import 'listview_horizontal_widget/list_horizontal_higest_prices.dart';
 
-class ExchangeRatesBody extends StatefulWidget {
+class ExchangeRatesBody extends StatelessWidget {
   const ExchangeRatesBody({super.key});
-
-  @override
-  State<ExchangeRatesBody> createState() => _ExchangeRatesBodyState();
-}
-
-class _ExchangeRatesBodyState extends State<ExchangeRatesBody> {
 
   @override
   Widget build(BuildContext context) {
